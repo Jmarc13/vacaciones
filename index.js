2aa59b134855d71a87a27eeb9632772fc78966ab
PixelPerfection <129969641+Jmarc13@users.noreply.github.com>
2023-09-06 08:56:33 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function redirectSeguir(){
+    window.location.href = "/seguir.html";
+}
+
+function SeguirSi(){
+    window.location.href = "/seguir2.html"
+}
+function Seguir2Si(){
+    window.location.href="/portaventura.html"
+}
+function SeguirNo(){
+    window.location.href = "/seguirNo.html"
+}
+function home(){
+    window.location.href="index.html"
+}
+function extras(){
+    window.location.href="extras.html"
+}
+function salir(){
+    window.location.href="index.html"
+}
